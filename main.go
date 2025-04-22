8d73bc99f4876bf7e54e58896995ae89550de14e
Steve Xia <stevexia@futunn.com>
2025-04-22 23:29:50 +0800

    增加自动清理，更新相关库版本

--- main.go
+++ main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"flag"
 	"fmt"
-	"github.com/gin-gonic/gin"
-	"github.com/google/pprof/driver"
 	"io"
 	"net/http"
 	_ "net/http/pprof"
@@ -12,6 +10,10 @@ import (
 	"path/filepath"
 	"strings"
 	"sync"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/google/pprof/driver"
 )
 
 type nullUI struct{}
@@ -43,43 +45,74 @@ var _ driver.UI = (*nullUI)(nil)
 
 type PProfHandler struct {
 	args *driver.HTTPServerArgs
+	time time.Time
 }
 
-var profileMap sync.Map // map[string]*PProfHandler
+var (
+	profileMap     sync.Map
+	mu             sync.Mutex
+	profileTTL     time.Duration = 30 * time.Minute // 设置profile缓存30分钟
+	watcherRunning bool
+)
 
-var mu sync.Mutex
-
-func getHandler(fp string) (*PProfHandler, error) {
-	var ph *PProfHandler
-	if v, loaded := profileMap.Load(fp); loaded {
-		ph = v.(*PProfHandler)
-		return ph, nil
+// 新增清理函数
+func cleanupProfiles() {
+	for {
+		time.Sleep(5 * time.Minute) // 每5分钟检查一次
+		profileMap.Range(func(key, value interface{}) bool {
+			if ph, ok := value.(*PProfHandler); ok {
+				if time.Since(ph.time) > profileTTL {
+					profileMap.Delete(key)
+				}
+			}
+			return true
+		})
 	}
-	// generate http handlers
+}
+
+// 修改后的getHandler函数
+func getHandler(fp string) (*PProfHandler, error) {
+	// 检查文件修改时间
+	info, err := os.Stat(fp)
+	if err != nil {
+		return nil, err
+	}
+
+	if v, loaded := profileMap.Load(fp); loaded {
+		ph := v.(*PProfHandler)
+		// 检查文件是否被修改
+		if info.ModTime().After(ph.time) {
+			profileMap.Delete(fp) // 文件已修改，删除旧缓存
+		} else {
+			return ph, nil
+		}
+	}
+
 	mu.Lock()
 	defer mu.Unlock()
 
+	// 再次检查，防止并发创建
 	if v, loaded := profileMap.Load(fp); loaded {
-		ph = v.(*PProfHandler)
-		return ph, nil
+		return v.(*PProfHandler), nil
 	}
 
-	ph = &PProfHandler{}
+	ph := &PProfHandler{}
 	opts := &driver.Options{
 		UI: &nullUI{},
 		HTTPServer: func(args *driver.HTTPServerArgs) error {
+			ph.time = time.Now() // 记录加载时间
 			ph.args = args
 			return nil
 		},
 		HTTPTransport: http.DefaultTransport,
 		Flagset:       NewGoFlags([]string{"-http", ":8888", "--no_browser", fp}),
 	}
+
 	if err := driver.PProf(opts); err != nil {
 		return nil, err
 	}
 
 	profileMap.Store(fp, ph)
-
 	return ph, nil
 }
 
@@ -101,6 +134,9 @@ func main() {
 	if addr == "" {
 		addr = ":26817"
 	}
+
+	// 在main函数开始时启动清理goroutine
+	go cleanupProfiles()
 
 	r := gin.Default()
 
